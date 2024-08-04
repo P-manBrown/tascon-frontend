aea7b32f52351d2cb4c7a847c869229043f47a47
P-manBrown <80106172+P-manBrown@users.noreply.github.com>
2024-08-04 12:00:39 +0900

    chore: fix error when running Prettier

--- .prettierrc.js
+++ .prettierrc.js
@@ -1,5 +1,6 @@
+// TEMP: https://is.gd/ja0J4b
 module.exports = {
   semi: false,
   singleQuote: true,
-  plugins: [require('prettier-plugin-tailwindcss')],
+  plugins: [require.resolve('prettier-plugin-tailwindcss')],
 }
