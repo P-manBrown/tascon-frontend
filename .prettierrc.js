0e0484ee77163dc106669a499d2387da9173cac1
P-manBrown <80106172+P-manBrown@users.noreply.github.com>
2024-08-05 15:21:33 +0900

    chore: revert temporary workaround for Prettier
    Removed the workaround in the Prettier configuration file as it is no
    longer necessary after changing the installation mode of Yarn to
    `node-modules`.
    
    Refs: #139, #143

--- .prettierrc.js
+++ .prettierrc.js
@@ -1,6 +1,5 @@
-// TEMP: https://is.gd/ja0J4b
 module.exports = {
   semi: false,
   singleQuote: true,
-  plugins: [require.resolve('prettier-plugin-tailwindcss')],
+  plugins: ['prettier-plugin-tailwindcss'],
 }
