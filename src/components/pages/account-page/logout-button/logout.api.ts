8ea26dd1f788d91076e664e8a0d9279c20d7a332
P-manBrown <80106172+P-manBrown@users.noreply.github.com>
2024-10-20 19:29:58 +0900

    refactor: change the implementation of the logout button to use Server Actions
    Refs: #296

--- src/components/pages/account-page/logout-button/logout.api.ts
+++ src/components/pages/account-page/logout-button/logout.api.ts
@@ -1,18 +1,53 @@
+'use server'
+
+import { cookies } from 'next/headers'
+import { redirect } from 'next/navigation'
+import { z } from 'zod'
+import { ResultObject } from '@/types/api'
 import { fetchData } from '@/utils/api/fetch-data'
+import { proxyServerCookies } from '@/utils/cookie/proxy-server-cookies'
+import { createErrorObject } from '@/utils/error/create-error-object'
+import { getRequestId } from '@/utils/request-id/get-request-id'
+import { validateData } from '@/utils/validation/validate-data'
+
+const dataSchema = z.object({
+  success: z.literal(true),
+})
+
+type Data = z.infer<typeof dataSchema>
 
 export async function logout(csrfToken: string) {
-  const result = await fetchData(
+  const fetchDataResult = await fetchData(
     `${process.env.NEXT_PUBLIC_API_ORIGIN}/api/v1/auth/sign_out`,
     {
       method: 'DELETE',
       headers: {
         'X-CSRF-Token': csrfToken,
+        Cookie: cookies().toString(),
       },
-      credentials: 'include',
-    }
+    },
   )
 
-  if (result instanceof Error) {
-    return result
+  let resultObject: ResultObject<Data>
+
+  if (fetchDataResult instanceof Error) {
+    resultObject = createErrorObject(fetchDataResult)
+  } else {
+    const { headers, data } = fetchDataResult
+    const requestId = getRequestId(headers)
+    const validateDataResult = validateData({ requestId, dataSchema, data })
+
+    if (validateDataResult instanceof Error) {
+      resultObject = createErrorObject(validateDataResult)
+    } else {
+      resultObject = {
+        status: 'success',
+        ...validateDataResult,
+      }
+      proxyServerCookies(headers)
+      redirect('/')
+    }
   }
+
+  return resultObject
 }
