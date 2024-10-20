b2323fca885eff71e1978474c96617c1c5cda470
P-manBrown <80106172+P-manBrown@users.noreply.github.com>
2024-10-20 17:18:52 +0900

    fix: fix the layout shift on the account page (#295)

--- src/components/pages/account-page/index.tsx
+++ src/components/pages/account-page/index.tsx
@@ -1,11 +1,10 @@
 import { redirect } from 'next/navigation'
-import { Suspense } from 'react'
 import { HorizontalRule } from '@/components/horizontal-rule'
 import { getCsrfToken } from '@/utils/cookie/get-csrf-token'
 import { getCurrentUserId } from '@/utils/cookie/get-current-user-id'
 import { generateRedirectLoginPath } from '@/utils/login-path/generate-redirect-login-path.server'
 import { AccountQueryParamSnackbar } from './account-query-param-snackbar'
-import { CurrentUserInfo, LoadingCurrentUserInfo } from './current-user-info'
+import { CurrentUserInfo } from './current-user-info'
 import { DeleteAccountButton } from './delete-account-button'
 import { LogoutButton } from './logout-button'
 
@@ -25,9 +24,7 @@ export function AccountPage() {
 
   return (
     <div className="flex flex-col gap-y-10">
-      <Suspense fallback={<LoadingCurrentUserInfo />}>
-        <CurrentUserInfo />
-      </Suspense>
+      <CurrentUserInfo />
       <HorizontalRule />
       <LogoutButton csrfToken={csrfToken} />
       <DeleteAccountButton
