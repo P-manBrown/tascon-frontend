561295394363cc87348675be4d5a53149a71b71a
P-manBrown <80106172+P-manBrown@users.noreply.github.com>
2024-10-24 18:11:15 +0900

    refactor: change the implementation of the login link to use Server Actions (#306)

---
+++ src/app/_components/login-link/validate-token.api.ts
@@ -0,0 +1,50 @@
+'use server'
+
+import camelcaseKeys, { CamelCaseKeys } from 'camelcase-keys'
+import { cookies } from 'next/headers'
+import { z } from 'zod'
+import { validateTokenDataSchema } from '@/schemas/response/validate-token-success'
+import { ResultObject } from '@/types/api'
+import { fetchData } from '@/utils/api/fetch-data'
+import { createErrorObject } from '@/utils/error/create-error-object'
+import { getRequestId } from '@/utils/request-id/get-request-id'
+import { validateData } from '@/utils/validation/validate-data'
+
+type Data = CamelCaseKeys<z.infer<typeof validateTokenDataSchema>, true>
+
+export async function validateToken() {
+  const fetchDataResult = await fetchData(
+    `${process.env.API_ORIGIN}/api/v1/auth/validate_token`,
+    {
+      method: 'GET',
+      headers: {
+        Cookie: cookies().toString(),
+      },
+    },
+  )
+
+  let resultObject: ResultObject<Data>
+
+  if (fetchDataResult instanceof Error) {
+    resultObject = createErrorObject(fetchDataResult)
+  } else {
+    const { headers, data } = fetchDataResult
+    const requestId = getRequestId(headers)
+    const validateDataResult = validateData({
+      requestId,
+      dataSchema: validateTokenDataSchema,
+      data,
+    })
+
+    if (validateDataResult instanceof Error) {
+      resultObject = createErrorObject(validateDataResult)
+    } else {
+      resultObject = {
+        status: 'success',
+        ...camelcaseKeys(validateDataResult, { deep: true }),
+      }
+    }
+  }
+
+  return resultObject
+}
