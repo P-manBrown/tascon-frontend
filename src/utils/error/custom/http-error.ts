48da5b724dcc110ee95aa05eea890e3c58852380
P-manBrown <80106172+P-manBrown@users.noreply.github.com>
2024-10-29 14:54:48 +0900

    refactor: remove status from HttpError (#321)

--- src/utils/error/custom/http-error.ts
+++ src/utils/error/custom/http-error.ts
@@ -8,13 +8,11 @@ export class HttpError extends Error {
   name: 'HttpError'
   requestId: string
   statusCode: number
-  status: number // TODO: Remove this property in the future
 
   constructor({ requestId, res, message }: Params) {
     super(message ?? 'リクエストの処理中にエラーが発生しました。')
     this.name = 'HttpError'
     this.requestId = requestId
     this.statusCode = res.status
-    this.status = res.status // TODO: Remove this property in the future
   }
 }
