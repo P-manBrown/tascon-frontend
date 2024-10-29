307ca45ebcdba5af556dc4612665c4a88f2777d3
P-manBrown <80106172+P-manBrown@users.noreply.github.com>
2024-10-29 12:09:04 +0900

    refactor: remove unused currentUserId from AccountPage
    Refs: #316

--- src/components/pages/account-page/index.tsx
+++ src/components/pages/account-page/index.tsx
@@ -1,9 +1,6 @@
-import { redirect } from 'next/navigation'
 import { Suspense } from 'react'
 import { HorizontalRule } from '@/components/horizontal-rule'
 import { getCsrfToken } from '@/utils/cookie/get-csrf-token'
-import { getCurrentUserId } from '@/utils/cookie/get-current-user-id'
-import { generateRedirectLoginPath } from '@/utils/login-path/generate-redirect-login-path.server'
 import { AccountQueryParamSnackbar } from './account-query-param-snackbar'
 import { CurrentUserInfo } from './current-user-info'
 import {
@@ -13,13 +10,6 @@ import {
 import { LogoutButton } from './logout-button'
 
 export function AccountPage() {
-  const getCurrentUserIdResult = getCurrentUserId()
-  if (getCurrentUserIdResult instanceof Error) {
-    const redirectLoginPath = generateRedirectLoginPath()
-    redirect(redirectLoginPath)
-  }
-  const currentUserId = getCurrentUserIdResult
-
   const getCsrfTokenResult = getCsrfToken()
   if (getCsrfTokenResult instanceof Error) {
     throw getCsrfTokenResult
