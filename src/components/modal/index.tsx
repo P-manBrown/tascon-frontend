e40cc730df5ed0ca94f00e16c22055a2692fde56
P-manBrown <80106172+P-manBrown@users.noreply.github.com>
2025-05-05 19:43:47 +0900

    fix: correct the display position of the modal

--- src/components/modal/index.tsx
+++ src/components/modal/index.tsx
@@ -46,9 +46,9 @@ export function Modal({
   return (
     <dialog
       ref={ref}
-      className={`overflow-scroll rounded-md border border-gray-200 p-0 shadow-black/30 backdrop:bg-transparent backdrop:backdrop-blur-sm max-md:inset-auto max-md:bottom-0 max-md:left-0 max-md:max-h-[95%] max-md:w-full max-md:max-w-full md:shadow-lg ${
+      className={`m-auto overflow-scroll rounded-md border border-gray-200 p-0 shadow-black/30 backdrop:bg-transparent backdrop:backdrop-blur-sm max-md:inset-auto max-md:bottom-0 max-md:left-0 max-md:max-h-[95%] max-md:w-full max-md:max-w-full md:shadow-lg ${
         isOpen
-          ? 'overflow- animate-slide-in-bottom md:animate-scale-in-center'
+          ? 'animate-slide-in-bottom md:animate-scale-in-center'
           : 'animate-slide-out-bottom md:animate-scale-out-center'
       } ${!onBackdropClick ? 'backdrop:cursor-not-allowed' : ''}`}
       onMouseDown={onBackdropMouseDown}
