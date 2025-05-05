82ac306df9b714da7b829b071e56e17a8b92536e
P-manBrown <80106172+P-manBrown@users.noreply.github.com>
2025-05-05 19:38:32 +0900

    fix: correct the border color around the form on the sign-up page

--- src/app/(auth)/layout.tsx
+++ src/app/(auth)/layout.tsx
@@ -5,7 +5,7 @@ type Props = {
 export default function AuthLayout({ children }: Props) {
   return (
     <main className="md:flex md:min-h-dvh md:items-center">
-      <div className="mx-auto my-12 w-full max-w-md md:my-0 md:border md:shadow-lg md:shadow-black/25">
+      <div className="mx-auto my-12 w-full max-w-md md:my-0 md:border md:border-gray-200 md:shadow-lg md:shadow-black/25">
         <div className="mx-6 md:m-12">{children}</div>
       </div>
     </main>
