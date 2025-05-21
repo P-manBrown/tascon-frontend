6ad6481950bf7c35b36dce11b19f8b2af7a76e03
P-manBrown <80106172+P-manBrown@users.noreply.github.com>
2025-05-21 14:03:40 +0900

    fix: fix display of Spinner in Button

--- src/components/buttons/button/index.tsx
+++ src/components/buttons/button/index.tsx
@@ -20,8 +20,8 @@ export function Button({
       {...rest}
     >
       {status === 'pending' ? (
-        <span className="flex h-full w-full items-center justify-center">
-          <Spinner className="mr-3 h-2/5 min-h-3.5 w-auto border-[3.5px] border-current" />
+        <span className="flex h-full w-full items-center justify-center gap-1">
+          <Spinner className="size-2/3" />
           処理中...
         </span>
       ) : (
