c1ad174b1c0f76e5a082f46172c23fc550bca4eb
P-manBrown <80106172+P-manBrown@users.noreply.github.com>
2025-08-07 19:27:40 +0900

    fix: prevent main content wrapping on sidebar toggle in mobile view (#748)

--- src/app/(main)/users/layout.tsx
+++ src/app/(main)/users/layout.tsx
@@ -8,13 +8,13 @@ type Props = {
 
 export default async function UsersLayout({ children, modal }: Props) {
   return (
-    <div className="flex h-full w-full">
-      <div className="flex-none">
-        <Suspense fallback={null}>
-          <UsersSidebar />
-        </Suspense>
-      </div>
-      <main className="flex-grow overflow-auto p-4">{children}</main>
+    <div className="flex h-full">
+      <Suspense fallback={null}>
+        <UsersSidebar />
+      </Suspense>
+      <main className="flex-grow overflow-auto p-4 max-md:min-w-dvw">
+        {children}
+      </main>
       {modal}
     </div>
   )
