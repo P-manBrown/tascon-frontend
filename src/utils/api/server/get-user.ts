5a56a71f1fdc95dfc22a83120be70eb675706036
P-manBrown <80106172+P-manBrown@users.noreply.github.com>
2025-08-18 18:30:22 +0900

    fix: display not found UI for user profile page
    Fix to ensure Next.js notFound() is properly called and NotFoundUI is displayed when user does not exist.

--- src/utils/api/server/get-user.ts
+++ src/utils/api/server/get-user.ts
@@ -1,8 +1,11 @@
 import 'server-only'
 import camelcaseKeys from 'camelcase-keys'
+import { notFound, redirect } from 'next/navigation'
 import { cache } from 'react'
 import { z } from 'zod'
 import { getBearerToken } from '@/utils/cookie/bearer-token'
+import { HttpError } from '@/utils/error/custom/http-error'
+import { generateRedirectLoginPath } from '@/utils/login-path/generate-redirect-login-path.server'
 import { getRequestId } from '../../request-id/get-request-id'
 import { validateData } from '../../validation/validate-data'
 import { fetchData } from '../fetch-data'
@@ -33,8 +36,19 @@ export const getUser = cache(async (id: string) => {
       },
     },
   )
+
   if (fetchDataResult instanceof Error) {
-    return fetchDataResult
+    const isHttpError = fetchDataResult instanceof HttpError
+    const isUnauthorized = isHttpError && fetchDataResult.statusCode === 401
+    const isNotFound = isHttpError && fetchDataResult.statusCode === 404
+    if (isUnauthorized) {
+      const redirectLoginPath = await generateRedirectLoginPath()
+      redirect(redirectLoginPath)
+    }
+    if (isNotFound) {
+      notFound()
+    }
+    throw fetchDataResult
   }
 
   const { headers, data } = fetchDataResult
@@ -42,7 +56,7 @@ export const getUser = cache(async (id: string) => {
 
   const validateDataResult = validateData({ requestId, dataSchema, data })
   if (validateDataResult instanceof Error) {
-    return validateDataResult
+    throw validateDataResult
   }
 
   const camelcaseData = camelcaseKeys(validateDataResult, { deep: true })
